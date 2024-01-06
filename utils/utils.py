f5c179d574669b8fad9859cac96af9068d5e8ca5
Yoni <talyonatantal@gmail.com>
2024-01-07 01:10:27 +0200

    Added method to format Solution object into json file

--- utils/utils.py
+++ utils/utils.py
@@ -145,6 +145,7 @@ def visualize_solution(container, shapes,solution):
 # Example usage:
 CONTAINER,SHAPES_LIST = load_json_from_file('../data/atris42.cgshop2024_instance.json','i')
 SOLUTION = load_json_from_file('../data/artis42.cgshop2024_items_overlap_solution.json','s')
+#print(SOLUTION.export_to_json())
 valid = is_valid_solution(CONTAINER,SHAPES_LIST,SOLUTION)
 print("Is the solution valid?", valid)
 
